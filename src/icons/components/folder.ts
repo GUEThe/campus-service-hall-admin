35d71a82f3567dd202e0b32a78bf4de1dc515001
wilsunson <839785143@qq.com>
2019-05-10 15:01:14 +0800

    手动添加svg方法

---
+++ src/icons/components/folder.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+// @ts-ignore
+import icon from 'vue-svgicon'
+icon.register({
+  'folder': {
+    width: 128,
+    height: 128,
+    viewBox: '0 0 1024 1024',
+    data: '<defs/><path d="M847.3 702.3H176.7V300.8h620.7c27.6 0 50 22.4 50 50v351.5h-0.1zM595.8 300.8c0-46.3-37.5-83.8-83.8-83.8H176.7v83.8h419.1" fill="#CE9F06" p-id="4562"></path><path d="M260.5 590.4V339h503v251.4h-503z" fill="#FFFFFF" p-id="4563"></path><path d="M847.3 755.4V429.8c0-23.1-18.8-41.9-41.9-41.9H218.6c-23.1 0-41.9 18.8-41.9 41.9v325.5c0 28.5 23.1 51.7 51.7 51.7h567.3c28.5 0.1 51.6-23.1 51.6-51.6z" fill="#FFCD2C" p-id="4564" />'
+  }
+})
